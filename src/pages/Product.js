7001e47c0cc6400ec83ca27916e7306542bb5da3
sama malmir <malmir.sama1@gmail.com>
2023-11-23 10:04:47 +0330

    use axios

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React,{useState, useEffect } from "react";
+import { Link , useParams} from "react-router-dom";
+import { Row, Col, Image, ListGroup, Button } from "react-bootstrap";
+import axios from "axios";
 
-const Product = () => {
+
+
+const Product = ({ match }) => {
+    const [product ,setProduct] = useState({})
+    const {id} = useParams()
+    useEffect(()=> {
+        
+        const sendRequest = async () => {
+            const response= await axios.get(`http://localhost:8000/api/products/${id}`)
+            setProduct(response.data)
+        }
+        sendRequest()
+    },[match])
+
   return (
     <div>
-      product
-    </div>
-  )
-}
+      <Link to="/" className="btn btn-light my-3">
+        بازگشت به صفحه اصلی
+      </Link>
 
-export default Product
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+                <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+                {product.price}
+            </ListGroup.Item>
+            <ListGroup.Item>
+                {product.description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+            <ListGroup variant="flush">
+                <ListGroup.Item>
+                    <Button className="btn-block" type="button">
+                        افزودن به سبد خرید
+                    </Button>
+                </ListGroup.Item>
+            </ListGroup>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default Product;
